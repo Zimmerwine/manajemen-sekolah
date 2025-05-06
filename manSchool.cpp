0bbb4f9ef9083d06945dc726485060dce631cb95
Zimmerwine <kakakecil67@gmail.com>
2025-05-06 18:13:51 +0700

    ubah banyak

--- manSchool.cpp
+++ manSchool.cpp
@@ -2,9 +2,12 @@
 #include <iomanip>
 #include <string>
 using namespace std;
-int jumlahSiswa = 0; 
+
+// Variabel global
+int jumlahSiswa = 0;
 int jumlahGuru = 0;
 
+// Struct
 struct Datasiswa {
     string nama;
     int noAbsen;
@@ -16,7 +19,6 @@ struct Datasiswa {
 
 struct Dataguru {
     string nama;
-    int npdn;
     int npdn;
     string alamat;
     int lamaMengajar;
@@ -38,6 +40,7 @@ struct Sekolah {
 
 Sekolah sekolah;
 
+// Fungsi input data siswa
 void inputSiswa() {
     cout << "Masukkan jumlah siswa: ";
     cin >> jumlahSiswa;
@@ -60,11 +63,10 @@ void inputSiswa() {
     }
 }
 
+// Fungsi tampil data siswa
 void tampilSiswa() {
-    if (jumlahSiswa == 0)
-    {
-    cout<<"Data Siswa Belum Diinput\n";
-    cout<<"Silahkan Input Data Terlebih Dahulu\n";
+    if (jumlahSiswa == 0) {
+        cout << "Data Siswa Belum Diinput\nSilahkan Input Data Terlebih Dahulu\n";
     } else {
         cout << "\nData Siswa:\n";
         for (int i = 0; i < jumlahSiswa; i++) {
@@ -73,22 +75,9 @@ void tampilSiswa() {
                  << " | Kelas: " << sekolah.siswa[i].kelas << " | Alamat: " << sekolah.siswa[i].alamat << endl;
         }
     }
-    
-    if (jumlahSiswa == 0)
-    {
-    cout<<"Data Siswa Belum Diinput\n";
-    cout<<"Silahkan Input Data Terlebih Dahulu\n";
-    } else {
-        cout << "\nData Siswa:\n";
-        for (int i = 0; i < jumlahSiswa; i++) {
-            cout << "Nama: " << sekolah.siswa[i].nama << " | No Absen: " << sekolah.siswa[i].noAbsen
-                 << " | NIS: " << sekolah.siswa[i].nis << " | NISN: " << sekolah.siswa[i].nisn
-                 << " | Kelas: " << sekolah.siswa[i].kelas << " | Alamat: " << sekolah.siswa[i].alamat << endl;
-        }
-    }
-    
 }
 
+// Fungsi input data guru
 void inputGuru() {
     cout << "Masukkan jumlah guru: ";
     cin >> jumlahGuru;
@@ -98,8 +87,8 @@ void inputGuru() {
         cout << "Nama: ";
         getline(cin, sekolah.guru[i].nama);
         cout << "NPDN: ";
-        cin>>sekolah.guru[i].npdn;
-        cin>>sekolah.guru[i].npdn;
+        cin >> sekolah.guru[i].npdn;
+        cin.ignore();
         cout << "Alamat: ";
         getline(cin, sekolah.guru[i].alamat);
         cout << "Lama Mengajar (tahun): ";
@@ -112,32 +101,15 @@ void inputGuru() {
     }
 }
 
+// Fungsi tampil data guru
 void tampilGuru() {
-    if (jumlahGuru == 0)
-    {
-    cout<<"Data Guru Belum Diinput\n";
-    cout<<"Silahkan Input Data Terlebih Dahulu\n";
+    if (jumlahGuru == 0) {
+        cout << "Data Guru Belum Diinput\nSilahkan Input Data Terlebih Dahulu\n";
     } else {
         cout << "\nData Guru:\n";
         for (int i = 0; i < jumlahGuru; i++) {
             cout << "Nama: " << sekolah.guru[i].nama << " | NPDN: " << sekolah.guru[i].npdn
-                 << " | Alamat: " << sekolah.guru[i].alamat << " | Lama Mengajar: " << sekolah.guru[i].lamaMengajar << " tahun" << endl;
-            cout << "Mata Pelajaran: ";
-            for (int j = 0; j < 3; j++) {
-                cout << sekolah.guru[i].mapel[j] << (j < 2 ? ", " : "");
-            }
-            cout << endl;
-        }
-    }
-    if (jumlahGuru == 0)
-    {
-    cout<<"Data Guru Belum Diinput\n";
-    cout<<"Silahkan Input Data Terlebih Dahulu\n";
-    } else {
-        cout << "\nData Guru:\n";
-        for (int i = 0; i < jumlahGuru; i++) {
-            cout << "Nama: " << sekolah.guru[i].nama << " | NPDN: " << sekolah.guru[i].npdn
-                 << " | Alamat: " << sekolah.guru[i].alamat << " | Lama Mengajar: " << sekolah.guru[i].lamaMengajar << " tahun" << endl;
+                 << " | Alamat: " << sekolah.guru[i].alamat << " | Lama Mengajar: " << sekolah.guru[i].lamaMengajar << " tahun\n";
             cout << "Mata Pelajaran: ";
             for (int j = 0; j < 3; j++) {
                 cout << sekolah.guru[i].mapel[j] << (j < 2 ? ", " : "");
@@ -147,8 +119,9 @@ void tampilGuru() {
     }
 }
 
+// Fungsi pencarian siswa (sentinel search)
 void cariSiswaSentinel(int jumlahSiswa, int nis) {
-    sekolah.siswa[jumlahSiswa].nis = nis; 
+    sekolah.siswa[jumlahSiswa].nis = nis;
     int i = 0;
     while (sekolah.siswa[i].nis != nis) {
         i++;
@@ -159,12 +132,9 @@ void cariSiswaSentinel(int jumlahSiswa, int nis) {
         cout << "\nData Siswa tidak ditemukan!" << endl;
 }
 
-void cariGuruLinear(int jumlahGuru, int npdn) {
+// Fungsi pencarian guru (linear search)
 void cariGuruLinear(int jumlahGuru, int npdn) {
     bool ditemukan = false;
-    for (int i = 0; i < jumlahGuru; i++) {
-        if (sekolah.guru[i].npdn == npdn) {
-            cout << "\nData Guru ditemukan: " << sekolah.guru[i].npdn << " dengan nama " << sekolah.guru[i].nama << endl;
     for (int i = 0; i < jumlahGuru; i++) {
         if (sekolah.guru[i].npdn == npdn) {
             cout << "\nData Guru ditemukan: " << sekolah.guru[i].npdn << " dengan nama " << sekolah.guru[i].nama << endl;
@@ -174,36 +144,9 @@ void cariGuruLinear(int jumlahGuru, int npdn) {
     }
     if (!ditemukan)
         cout << "\nData Guru tidak ditemukan!" << endl;
-        cout << "\nData Guru tidak ditemukan!" << endl;
-}
-void menuInput(){
-    int menuInput;
-    do{
-    system("cls");
-    cout << "+" << setw(50) << setfill('=') << "+" << endl;
-    cout << "|" << setw(32) << setfill(' ') << "MENU INPUT DATA" << setw(18) << "|" << endl;
-    cout << "+" << setw(50) << setfill('=') << "+" << endl;
-    cout << "|Pilihan Menu : " << setw(35) << setfill(' ') << "|" << endl;
-    cout << "|1. Data Siswa  " << setw(35) << setfill(' ') << "|" << endl;
-    cout << "|2. Data Guru   " << setw(35) << setfill(' ') << "|" << endl;
-    cout << "+" << setw(50) << setfill('=') << "+" << endl;
-    cout << "Masukkan Pilihan Menu : ";cin>>menuInput;
-    if (menuInput==1)
-    {
-    inputSiswa();
-    } else if (menuInput==2)
-    {
-    inputGuru();
-    }
-    if (menuInput!=1&&menuInput!=2)
-    {
-    cout<<"Input yang anda masukkan salah"<<endl;
-    system("pause");
-    }
-    
-    } while (menuInput!=1&&menuInput!=2);
 }
 
+// Fungsi untuk sorting data siswa
 void bubbleSortSiswa() {
     for (int i = 0; i < jumlahSiswa - 1; i++) {
         for (int j = 0; j < jumlahSiswa - i - 1; j++) {
@@ -215,123 +158,133 @@ void bubbleSortSiswa() {
     cout << "\nData siswa telah diurutkan berdasarkan NIS (ascending).\n";
     system("pause");
 }
-void menuOutput(){
-    int menuOutput;
-    do{
-    system("cls");
-    cout << "+" << setw(50) << setfill('=') << "+" << endl;
-    cout << "|" << setw(33) << setfill(' ') << "MENU OUTPUT DATA" << setw(17) << "|" << endl;
-    cout << "+" << setw(50) << setfill('=') << "+" << endl;
-    cout << "|Pilihan Menu : " << setw(35) << setfill(' ') << "|" << endl;
-    cout << "|1. Data Siswa  " << setw(35) << setfill(' ') << "|" << endl;
-    cout << "|2. Data Guru   " << setw(35) << setfill(' ') << "|" << endl;
-    cout << "+" << setw(50) << setfill('=') << "+" << endl;
-    cout << "Masukkan Pilihan Menu : ";cin>>menuOutput;
-    if (menuOutput==1){
-        bubbleSortSiswa();
-        tampilSiswa();
-    }else if (menuOutput==2)
-    {
-        tampilGuru();
-    }else if (menuOutput!=1&&menuOutput!=2)
-    {
-    cout<<"Input yang anda masukkan salah"<<endl;
-    system("pause");
-    }
-    }while (menuOutput!=1&&menuOutput!=2);
-}
 
-void menuCariSiswa(){
-    int inputnis;
-    system("cls");
-    cout << "+" << setw(50) << setfill('=') << "+" << endl;
-    cout << "|" << setw(33) << setfill(' ') << "MENU PENCARIAN DATA SISWA" << setw(17) << "|" << endl;
-    cout << "+" << setw(50) << setfill('=') << "+" << endl;
-    cout << "Masukkan NIS Siswa : ";cin>>inputnis;
-    cariSiswaSentinel(jumlahSiswa,inputnis);
-}
-
-void menuCariGuru(){
-    int inputnpdn;
-    system("cls");
-    cout << "+" << setw(50) << setfill('=') << "+" << endl;
-    cout << "|" << setw(33) << setfill(' ') << "MENU PENCARIAN DATA GURU " << setw(17) << "|" << endl;
-    cout << "+" << setw(50) << setfill('=') << "+" << endl;
-    cout << "Masukkan NPDN Guru : ";cin>>inputnpdn;
-    cariGuruLinear(jumlahGuru,inputnpdn);
-}
-
-void menuCari(){
-        int menuCari;
-        do{
+// Menu input
+void menuInput() {
+    int menuInput;
+    do {
         system("cls");
         cout << "+" << setw(50) << setfill('=') << "+" << endl;
-        cout << "|" << setw(33) << setfill(' ') << "MENU PENCARIAN DATA" << setw(17) << "|" << endl;
+        cout << "|" << setw(32) << setfill(' ') << "MENU INPUT DATA" << setw(18) << "|" << endl;
         cout << "+" << setw(50) << setfill('=') << "+" << endl;
         cout << "|Pilihan Menu : " << setw(35) << setfill(' ') << "|" << endl;
         cout << "|1. Data Siswa  " << setw(35) << setfill(' ') << "|" << endl;
         cout << "|2. Data Guru   " << setw(35) << setfill(' ') << "|" << endl;
         cout << "+" << setw(50) << setfill('=') << "+" << endl;
-        cout << "Masukkan Pilihan Menu : ";cin>>menuCari;
-        if (menuCari==1){
-        menuCariSiswa();
-        }else if (menuCari==2){
-        menuCariGuru();
-        }else if (menuCari!=1&&menuCari!=2)
-        {
-        cout<<"Input yang anda masukkan salah"<<endl;
-        system("pause");
+        cout << "Masukkan Pilihan Menu : ";cin>>menuInput;
+        switch (menuInput) {
+            case 1: inputSiswa(); break;
+            case 2: inputGuru(); break;
+            default: cout << "Input yang anda masukkan salah\n"; system("pause"); break;
         }
-        }while (menuCari!=1&&menuCari!=2);
-    
+    } while (menuInput != 1 && menuInput != 2);
 }
 
+// Menu output
+void menuOutput() {
+    int menuOutput;
+    do {
+        system("cls");
+        cout << "+" << setw(50) << setfill('=') << "+" << endl;
+        cout << "|" << setw(33) << setfill(' ') << "MENU OUTPUT DATA" << setw(17) << "|" << endl;
+        cout << "+" << setw(50) << setfill('=') << "+" << endl;
+        cout << "|1. Data Siswa  |\n|2. Data Guru   |\n";
+        cout << "+" << setw(50) << setfill('=') << "+" << endl;
+        cout << "Masukkan Pilihan Menu : ";
+        cin >> menuOutput;
 
-void mainMenu(){
-    int menu;
-    do{
+        switch (menuOutput) {
+            case 1: bubbleSortSiswa(); tampilSiswa(); break;
+            case 2: tampilGuru(); break;
+            default: cout << "Input yang anda masukkan salah\n"; system("pause"); break;
+        }
+    } while (menuOutput != 1 && menuOutput != 2);
+}
+
+// Menu cari siswa
+void menuCariSiswa() {
+    int inputnis;
     system("cls");
     cout << "+" << setw(50) << setfill('=') << "+" << endl;
-    cout << "|" << setw(32) << setfill(' ') << "MANAJEMEN SEKOLAH" << setw(18) << "|" << endl;
+    cout << "|" << setw(33) << setfill(' ') << "MENU PENCARIAN DATA SISWA" << setw(17) << "|" << endl;
     cout << "+" << setw(50) << setfill('=') << "+" << endl;
-    cout << "|Pilihan Menu : " << setw(35) << setfill(' ') << "|" << endl;
-    cout << "|1. Input Data  " << setw(35) << setfill(' ') << "|" << endl;
-    cout << "|2. Output Data " << setw(35) << setfill(' ') << "|" << endl;
-    cout << "|3. Cari Data   " << setw(35) << setfill(' ') << "|" << endl;
-    cout << "|3. Cari Data   " << setw(35) << setfill(' ') << "|" << endl;
-    cout << "+" << setw(50) << setfill('=') << "+" << endl;
-    cout << "Masukkan Pilihan Menu : ";
-    cin >> menu;
-    if (menu == 1)
-    {
-    menuInput();
-    }else if (menu == 2){
-    menuOutput();
-    }else if (menu == 3){
-    menuCari();
-    }else if (menu !=1 && menu !=2 && menu !=3){
-    }else if (menu == 3){
-    menuCari();
-    }else if (menu !=1 && menu !=2 && menu !=3){
-    cout<<"Input yang anda masukkan salah"<<endl;
-    system("pause");
-    }
-    } while (menu !=1 && menu !=2 && menu !=3);
-    } while (menu !=1 && menu !=2 && menu !=3);
+    cout << "Masukkan NIS Siswa : ";
+    cin >> inputnis;
+    cariSiswaSentinel(jumlahSiswa, inputnis);
 }
 
+// Menu cari guru
+void menuCariGuru() {
+    int inputnpdn;
+    system("cls");
+    cout << "+" << setw(50) << setfill('=') << "+" << endl;
+    cout << "|" << setw(33) << setfill(' ') << "MENU PENCARIAN DATA GURU " << setw(17) << "|" << endl;
+    cout << "+" << setw(50) << setfill('=') << "+" << endl;
+    cout << "Masukkan NPDN Guru : ";
+    cin >> inputnpdn;
+    cariGuruLinear(jumlahGuru, inputnpdn);
+}
+
+// Menu cari data
+void menuCari() {
+    int menuCari;
+    do {
+        system("cls");
+        cout << "+" << setw(50) << setfill('=') << "+" << endl;
+        cout << "|" << setw(33) << setfill(' ') << "MENU PENCARIAN DATA" << setw(17) << "|" << endl;
+        cout << "+" << setw(50) << setfill('=') << "+" << endl;
+        cout << "|1. Data Siswa  |\n|2. Data Guru   |\n";
+        cout << "+" << setw(50) << setfill('=') << "+" << endl;
+        cout << "Masukkan Pilihan Menu : ";
+        cin >> menuCari;
+
+        switch (menuCari) {
+            case 1: menuCariSiswa(); break;
+            case 2: menuCariGuru(); break;
+            default: cout << "Input yang anda masukkan salah\n"; system("pause"); break;
+        }
+    } while (menuCari != 1 && menuCari != 2);
+}
+
+// Menu utama
+void mainMenu() {
+    int menu;
+    do {
+        system("cls");
+        cout << "+" << setw(50) << setfill('=') << "+" << endl;
+        cout << "|" << setw(32) << setfill(' ') << "MANAJEMEN SEKOLAH" << setw(18) << "|" << endl;
+        cout << "+" << setw(50) << setfill('=') << "+" << endl;
+        cout << "|Pilihan Menu : " << setw(35) << setfill(' ') << "|" << endl;
+        cout << "|1. Input Data  " << setw(35) << setfill(' ') << "|" << endl;
+        cout << "|2. Output Data " << setw(35) << setfill(' ') << "|" << endl;
+        cout << "|3. Cari Data   " << setw(35) << setfill(' ') << "|" << endl;
+        cout << "+" << setw(50) << setfill('=') << "+" << endl;
+        cout << "Masukkan Pilihan Menu : ";
+        cin >> menu;
+
+        switch (menu) {
+            case 1: menuInput(); break;
+            case 2: menuOutput(); break;
+            case 3: menuCari(); break;
+            default: cout << "Input yang anda masukkan salah\n"; system("pause"); break;
+        }
+    } while (menu != 1 && menu != 2 && menu != 3);
+}
+
+// Fungsi main
 int main() {
     string kembali;
-    do{
+    do {
         mainMenu();
-    do{
-        cout<<"Kembali Ke Menu?(y/n) : ";getline(cin>>ws,kembali);
-        if (kembali != "n" && kembali != "N" && kembali != "y" && kembali !="Y")
-        {
-            cout<<"Input yang anda masukkan salah"<<endl;
-            system("pause");
-        } 
-    } while (kembali != "n" && kembali != "N" && kembali != "y" && kembali !="Y");
-    }while (kembali == "y"||kembali=="Y");
+        do {
+            cout << "Kembali Ke Menu?(y/n) : ";
+            getline(cin >> ws, kembali);
+            if (kembali != "n" && kembali != "N" && kembali != "y" && kembali != "Y") {
+                cout << "Input yang anda masukkan salah\n";
+                system("pause");
+            }
+        } while (kembali != "n" && kembali != "N" && kembali != "y" && kembali != "Y");
+    } while (kembali == "y" || kembali == "Y");
 
+    return 0;
 }
